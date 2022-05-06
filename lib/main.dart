08853d41fe009e9c3c32fe692c3f59ed9922b334
Giyosjon Tursunov <g1y0sb3k@gmail.com>
2022-05-06 22:53:23 +0500

    dsa

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,16 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-// create MyHomePage widget
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Flutter Demo Home Page'),
-      ),
-      body: Center(
-        child: const Text('Hello World'),
+    int days = 30;
+    return Material(
+      child: Center(
+        child: Text('$days days'),
       ),
     );
   }
