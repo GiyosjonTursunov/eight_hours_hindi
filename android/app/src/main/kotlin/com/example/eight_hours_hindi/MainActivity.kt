0e420391f22f02713a19f9ac1f78ac4a4ce0a1d5
Giyosjon Tursunov <g1y0sb3k@gmail.com>
2022-05-06 22:49:02 +0500

    created eight_hours_project

---
+++ android/app/src/main/kotlin/com/example/eight_hours_hindi/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.eight_hours_hindi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+// create MyHomePage widget
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter Demo Home Page'),
+      ),
+      body: Center(
+        child: const Text('Hello World'),
+      ),
+    );
+  }
+}
