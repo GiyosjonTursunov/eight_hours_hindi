a788b71ed68c8535f58f5274a146cedb8ddaa14c
Giyosjon Tursunov <g1y0sb3k@gmail.com>
2022-05-11 18:28:02 +0500

    day 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:eight_hours_hindi/home_page.dart';
+import 'package:eight_hours_hindi/login_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -5,15 +7,34 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    int days = 30;
-    return Material(
-      child: Center(
-        child: Container(
-          child: Text('$days days'),
-        ),
+    bringVegetables(rupees: 50, thaila: true);
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      // home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
       ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      initialRoute: '/home',
+      routes: {
+        '/': (context) => LoginPage(),
+        '/home': (context) => HomePage(),
+        '/login': (context) => LoginPage(),
+      },
     );
+  }
+
+  bringVegetables({required bool thaila, int rupees = 30}) {
+    // Take Cycle
+
+    // Go to Sec 16
   }
 }
